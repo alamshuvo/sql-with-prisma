56461756f0d029393ebd3987cd3e16326e75409a
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-07 00:38:07 +0600

    upsert added

--- src/find.ts
+++ src/find.ts
@@ -6,14 +6,19 @@ const getAllFromDb = await prisma.post.findMany();
 //find one its findFrist find kore prothom j data ta match kore seita dyea dibe 
 const findFirst = await prisma.post.findFirstOrThrow({
     where:{
-        id: 9
+        id: 14
     }
 })
 
 // find unique holo unique vabe find kora 
 const findUnique = await prisma.post.findUniqueOrThrow({
     where:{
-        id: 9
+        id: 14
+    },
+    select:{
+        title:true,
+        content:true,
+        authorName:true
     }
 })
 
