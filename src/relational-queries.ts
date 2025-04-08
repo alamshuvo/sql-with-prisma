289b90605cd6818e9ec0f7c911261446e90d141a
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-08 15:48:26 +0600

    some code added

--- src/relational-queries.ts
+++ src/relational-queries.ts
@@ -1,13 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 
+
 const prisma = new PrismaClient();
 const relationalQueries = async () => {
     //fluent api 
-    const result = await prisma.user.findUnique({
-        where:{
-            id:1
+    // const result = await prisma.user.findUnique({
+    //     where:{
+    //         id:1
+    //     }
+    // }).profile()
+
+    // relational filters 
+    const publishedPostUsers = await prisma.user.findMany({
+        include:{
+            posts:{
+                where:{
+                    published:true
+                }
+            }
         }
-    }).profile()
-    console.log(result);
+    })
+    console.dir(publishedPostUsers,{depth:Infinity});
 }
 relationalQueries()
