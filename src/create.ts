5a4d7ec0d7fcd0d3383a69f16996ce0786c9182f
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-04-08 15:29:17 +0600

    some code added

--- src/create.ts
+++ src/create.ts
@@ -1,36 +1,45 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 const prisma = new PrismaClient();
 const main = async () => {
-  //   const result = await prisma.post.create({
-  //     data:{
-  //         title: 'Hello World this is title',
-  //         content: 'This is my first post',
-  //         authorName: 'Iftakhar Alam Shuvo',
-  //     }
-  //   })
-  //   console.log(result);
+// const createUser = await prisma.user.create({
+//   data:{
+//     userName:"user2",
+//     email:"user1@gmail.com",
+//     role:UserRole.user
+//   }
+// const createProfile = await prisma.profile.create({
+//   data:{
+//     bio:"this is bio",
+//     userId:2
+//   }
+// })
 
-  // create multiple data
-  const createMany = await prisma.post.createMany({
-    data: [
-      {
-        title: "Hello World this is title",
-        content: "This is my first post",
-        authorName: "Iftakhar",
-      },
-      {
-        title: "Hello World this is title",
-        content: "This is my second post",
-        authorName: "Alam",
-      },
-      {
-        title: "Hello World this is title",
-        content: "This is my third post",
-        authorName: "Shuvo",
-      }
-    ],
-  });
-    console.log(createMany);
+// const createCategory = await prisma.category.create({
+//   data:{
+//     name:"web_development",
+//   }
+// })
+const createPost = await prisma.post.create({
+    data:{
+        title:"this is title",
+        content:"this is content of the post",
+        authorId:1,
+        postCategory:{
+         create:{
+            // category:{
+            //     connect:{
+            //         id:1
+            //     }
+            // }
+            categoryId:3
+         }
+        }
+    },
+    include:{
+        postCategory:true
+    }
+})
+ console.log(createPost);
 };
 
 main();
